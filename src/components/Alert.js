5278b258d807f5a45f193ba391ce956688a0266c
primeravinder <primeravinder@gmail.com>
2025-05-29 16:49:54 +0530

    Successfully, fixed shifting of the text box on alert div functionality.

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,11 +6,13 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div className="container my-5">
-            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-                <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-            </div>}
-        </div>
+        <>
+            <div className="container" style={{height: '50px'}}>
+                {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                    <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+                </div>}
+            </div>
+        </>
     )
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,8 +38,7 @@ export default function TextForm(props) {
     }
 
     const [text, setText] = useState('');
-    // text = "new text"; // Wrong way to change the state
-    // setText("new text"); // Correct way to change the state
+
     return (
         <>
             <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
@@ -58,7 +57,7 @@ export default function TextForm(props) {
                 <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
                 <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Nothing to preview!"}</p>
+                <p>{text.length>0? text : "Nothing to preview!"}</p>
             </div>
         </>
     )
